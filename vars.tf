3e71a3c4a71a1eee2befd4d682f855bce2f1475e
Lakshmi <ln.ravipati@yahoo.com>
2018-05-24 17:58:32 +0000

    Consul cluster

---
+++ vars.tf
@@ -0,0 +1,9 @@
+variable "AWS_REGION" {
+  default = "us-west-2"
+}
+variable "LOCATION_PUBLIC_KEY" {
+  default = "terraform.key.pub"
+}
+variable "LOCATION_PRIVATE_KEY" {
+  default = "terraform.key"
+}
